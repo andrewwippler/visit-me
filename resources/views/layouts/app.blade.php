8d3dd9643f1834d426544e24bdcd7bd2ba921ccf
Andrew J Wippler <andrew.wippler@gmail.com>
2018-07-17 16:58:29 -0500

    basic UI - no coding yet :(

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,39 +5,30 @@
     <title>Visit Me</title>
     <meta content='width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no' name='viewport'>
 
-    <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
     <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-    <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/select2/4.0.2/css/select2.min.css">
-    <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/admin-lte/2.3.11/css/AdminLTE.min.css">
-    <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/admin-lte/2.3.11/css/skins/_all-skins.min.css">
 
-    <!-- Ionicons -->
-    <link rel="stylesheet" href="//code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">
-    <link rel="stylesheet" href="{{ mix('/css/all.css') }}">
+    <link rel="stylesheet" href="{{ mix('/css/app.css') }}">
 
     @yield('styles')
 </head>
 
-<body class="skin-blue-light sidebar-mini">
-    <div id="page-content-wrapper">
-        <div class="container-fluid">
-            <div class="row">
-                <div class="col-lg-12">
-                    @yield('content')
+<body>
+        <div id="wrap">
+                <div class="container">
+                  <div class="page-header">
+                    <h1>Visit Generator</h1>
+                  </div>
+                  @yield('content')
+              <div id="footer">
+                <div class="container">
+                  <p class="muted credit">
+                    &copy; <?php echo date("Y"); ?> Andrew Wippler
+                  </p>
                 </div>
-            </div>
-        </div>
-    </div>
-
+              </div>
     <!-- jQuery -->
     <script src="//code.jquery.com/jquery-1.12.4.js"></script>
     <script src="//code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
-    <script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
-    <script src="//cdnjs.cloudflare.com/ajax/libs/select2/4.0.2/js/select2.min.js"></script>
-    <script src="//cdnjs.cloudflare.com/ajax/libs/iCheck/1.0.2/icheck.min.js"></script>
-
-    <!-- AdminLTE App -->
-    <script src="//cdnjs.cloudflare.com/ajax/libs/admin-lte/2.3.11/js/app.min.js"></script>
 
     @yield('scripts')
 </body>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,7 +1,41 @@
 @extends('layouts.app')
 
 @section('content')
-<div>
-Hello World!
+<div class="col-sm-12">
+    <b>Please select a file</b>
+    <form name"fileform" method="post" enctype="multipart/form-data" action="maps.php">
+    <p><input type="file" name="theFile" size="20" /></p>
+    <input class="btn btn-large btn-primary" type="submit" name="submit" value="Create Maps" />
+    </form>
 </div>
+<br />
+<div class="alert alert-info">
+    <strong>UPDATED: </strong>File must be comma-separated text, with a header row, <br />
+    and contain the following required fields:
+</div>
+
+<div class="col-sm-12">
+    <p>Required Fields:</p>
+    <ul>
+        <li>First Name</li>
+        <li>Address Line 1</li>
+        <li>City</li>
+        <li>State</li>
+        <li>Zip</li>
+    </ul>
+    <p>
+    Optional Fields:
+    </p>
+    <ul>
+        <li>First Name</li>
+        <li>Last Name</li>
+        <li>Main/Home Phone</li>
+        <li>Address Line 2</li>
+        <li>Parent's name</li>
+        <li>Pickup time</li>
+        <li>Neighborhood</li> 
+    </ul>
+</div>
+    <div id="push"></div>
+
 @endsection
